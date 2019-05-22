9e41db8a8b7605953dd14da928ee316526bc09b4
kingrongH <kingronghuang@gmail.com>
2019-05-22 19:58:37 +0800

    move generate config function here and default proxy test

--- src/controller.ts
+++ src/controller.ts
@@ -10,6 +10,7 @@ import * as http from 'http';
 import { config as globalConfig } from './config';
 import { spawn } from 'child_process';
 import * as fs from 'fs';
+import { fromYaml,toYaml } from './decode';
 
 /**
  * select proxy while clash is running
@@ -23,7 +24,7 @@ export function proxySelect(name:string):Promise<boolean>{
 		name:name
 	};
 	if(globalConfig['external-controller']){
-		exCon = globalConfig['external-controller'];
+		exCon =`http://${globalConfig['external-controller']}` ;
 	}
 	return new Promise((resolve, reject)=>{
 		let req = http.request(exCon,{
@@ -111,15 +112,21 @@ export function killProcess(pid:string){
 	});
 }
 
-//return a clash child_process
-export function runClash(){
+export async function runClash(){
 	console.log('starting clash');
+	// gengerate config from the template for clash first
+	await generateClashConfig();
 	let log = fs.openSync(__dirname+'/../logs/clashLog.log','w');
 	let clash = spawn(__dirname+'/../source/clash-linux-amd64',['-d',globalConfig.clashConfigPath],{
 		stdio:['ignore',log,log],
 		detached:true
 	});
 	clash.unref();
+	// if there is default proxy set, change to that
+	// TODO: This totally need redesign
+	if(globalConfig.defaultProxy){
+		await proxySelect(globalConfig.defaultProxy);
+	}
 	console.log(`clash started at dir ${globalConfig.clashConfigPath}`);
 	console.log(`see the log ${globalConfig.clashConfigPath}`);
 }
@@ -130,4 +137,57 @@ export async function killClash(){
 		killProcess(pid);
 	}
 	console.log('all clash process were killed');
+}
+
+
+/**
+ * generate config for clash:
+ * 1. read startdard config from template
+ * 2. clash config's Proxy include our subscrible proxies
+ * 3. clash config's Proxy Group include our subscrible proxies name
+ * 4. other configs from global config 
+ */
+export async function generateClashConfig():Promise<void>{
+	let clashConfigStr = fs.readFileSync(__dirname + '/../source/config.yml',{encoding:'utf8'});
+	let clashConfig= fromYaml(clashConfigStr);
+	let proxyFiles = fs.readdirSync(globalConfig.proxiesPath);
+	//if there is many proxy files this could resolve many times
+	let proxyFilePromises:Promise<string>[] = []; 
+	for(let file of proxyFiles){
+		proxyFilePromises.push(new Promise((resolve, reject)=>{
+		fs.readFile(`${globalConfig.proxiesPath}/${file}`, {encoding:'utf8'}, (err,data)=>{
+				if(err){
+					reject(new Error(`Generating file err: ${err.message}`));
+				}else{
+					resolve(data);
+				}
+			});
+		}));
+	}
+	//对于clashConfig中在有的且全局配置里面也有的，需要将全局配置里的值赋给clashConfig中
+	for(let item in globalConfig){
+		if(clashConfig.hasOwnProperty(item)){
+			clashConfig[item] = globalConfig[item];
+		}
+	}
+	// use Promise.all() resolve all the data together
+	await Promise.all(proxyFilePromises).then((allData)=>{
+		for(let data of allData){
+			let proxies = fromYaml(data);
+			//if Proxy is initially null else concat proxies
+			if(!clashConfig.Proxy){
+				clashConfig.Proxy = proxies;
+			}else{
+				clashConfig.Proxy = clashConfig.Proxy.concat(proxies);
+			}
+			// add each proxy's  name to every group 
+			for(let x of clashConfig['Proxy Group']){
+				for(let y of proxies){
+					x.proxies.push(y.name);
+				}
+			}
+		}
+	});
+	let targetStr = toYaml(clashConfig);
+	fs.writeFileSync(globalConfig.clashConfigPath + '/config.yml', targetStr);
 }
